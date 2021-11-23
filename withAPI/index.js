fbcf6264743caf97d7c5d406d40f7181436c6d02
Josh <66495148+Joshwgaming@users.noreply.github.com>
2021-11-23 19:29:20 +0000

    Update index.js
    - Update code to work as a standalone bot
    - Update bot to work with discord.js v13.3.1 & node.js v16.13.0

--- withAPI/index.js
+++ withAPI/index.js
@@ -1,23 +1,28 @@
-const request = require('request');
-let badLinks = '';
+const { Client, Intents } = require('discord.js');
+const { botToken, muteRoleID, definedAPI } = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const fetch = require('node-fetch');
+let badLinks = ' ';
 
-client.on("message", async message => {
-    request("api link", function (error, response, body) {
-        if (error === null) {} else { console.log(`request error: ${error}`)};
-        badLinks = JSON.parse(body);
-    });
+client.login(botToken);
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => { 
 
     function autoMute(message, reason){
         const guild = client.guilds.cache.get(message.guild.id)
         const member = guild.members.cache.get(message.author.id)
-        member.roles.add("mute role id").catch(err => console.error);
+        member.roles.add(muteRoleID).catch(err => console.error);
     }
+    const badLinks = await fetch(definedAPI).then(response => response.json());
     if (message.author.id === client.user.id) {
     } else {
         if (badLinks.some(word => message.content.includes(word))) {
             autoMute(message, "Bad Link Usage")
-            message.reply("You have been muted for: **Bad Link Usage**")
-            message.delete()
+            message.channel.send(`${message.author.toString()} You have been muted for: **Bad Link Usage**`).catch(console.error).then(message.delete());
         }
     }
 })
