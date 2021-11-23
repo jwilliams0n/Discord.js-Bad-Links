f38e351cde10a7153577487df1ab581553340170
Josh <66495148+Joshwgaming@users.noreply.github.com>
2021-11-23 17:50:03 +0000

    Update index.js
    - Updating code to work as a standalone bot
    - Update bot to work with discord.js v13.3.1 & node.js v16.13.0

--- withJSON/index.js
+++ withJSON/index.js
@@ -1,17 +1,26 @@
+const { Client, Intents } = require('discord.js');
+const { botToken, muteRoleID } = require('./config.json');
 const badLinks = require('./badLinks.json')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.on("message", async message => {
+client.login(botToken);
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}`);
+});
+
+
+client.on("messageCreate", async (message) => { 
 
     function autoMute(userid, guildid, reason){
-        console.log(userid, guildid, reason)
         const guild = client.guilds.cache.get(guildid)
         const member = guild.members.cache.get(userid)
-        const role = member.guild.roles.cache.find(role => role.id === "mute role id")
+        const role = member.guild.roles.cache.find(role => role.id === muteRoleID)
         member.roles.add(role)
     }
 
     if (message.author.id === client.user.id) {
-      return;
+        return;
     } else {
         if (badLinks.some(word => message.content.includes(word))) {
             autoMute(message.author.id, message.guild.id, "Bad Link Usage")
